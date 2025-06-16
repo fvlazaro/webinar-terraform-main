c2abea2b46822b81bb4929493c6455de732f9c4a
Federico Valido <fvlazaro@gmail.com>
2025-06-16 23:00:20 +0200

    tags y outputs

--- main.tf
+++ main.tf
@@ -19,6 +19,13 @@ resource "aws_instance" "nginx-server" {
     vpc_security_group_ids = [
         aws_security_group.nginx-server-sg.id
     ]
+    tags = {
+        Name        = "nginx-server"
+        Environment = "test"
+        Owner       = "fvlazaro@gmail.com"
+        Team        = "DevOps"
+        Project     = "webinar"
+    }
 }
 ####### security group #####
 resource "aws_security_group" "nginx-server-sg" {
@@ -45,6 +52,13 @@ resource "aws_security_group" "nginx-server-sg" {
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
   }
+  tags = {
+        Name        = "nginx-server-sg"
+        Environment = "test"
+        Owner       = "fvlazaro@gmail.com"
+        Team        = "DevOps"
+        Project     = "webinar"
+    }
 }
 ####### ssh ####### 
 ####ssh-keygen -t rsa -b 2048 -f "nginx-server.key"
@@ -52,4 +66,20 @@ resource "aws_security_group" "nginx-server-sg" {
 resource "aws_key_pair" "nginx-server-ssh" {
   key_name   = "nginx-server-ssh"
   public_key = file("nginx-server.key.pub")
+  tags = {
+        Name        = "nginx-server-ssh"
+        Environment = "test"
+        Owner       = "fvlazaro@gmail.com"
+        Team        = "DevOps"
+        Project     = "webinar"
+    }
+}
+#######  output ####### 
+output "server_public_ip" {
+  description = "Dirección IP pública de la instancia EC2"
+  value       = aws_instance.nginx-server.public_ip
+}
+output "server_public_dns" {
+  description = "DNS público de la instancia EC2"
+  value       = aws_instance.nginx-server.public_dns
 }
