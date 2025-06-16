23c6d8e4710705187027624611099cb7bf8b02d2
Federico Valido <fvlazaro@gmail.com>
2025-06-16 22:39:29 +0200

    nginx +  ssh key + security group

--- main.tf
+++ main.tf
@@ -8,14 +8,48 @@ provider "aws" {
 resource "aws_instance" "nginx-server" {
     ami           = "ami-0440d3b780d96b29d"
     instance_type = "t3.micro"
-    #tags = {
-     #  Name        = "server-web"
-     #  Environment  = "test"
-     #  Owner         = "fvlazaro@gmail.com"
-      # Team         = "DevOps"
-    #   Project     = "webinar"
-    #}
-    #vpc_security_group_ids               = [
-    #    "sg-0d5b0d5e416f094c1",
-    #]
+    #instalacion de nginx
+    user_data = <<-EOF
+              #!/bin/bash
+              sudo yum install -y nginx
+              sudo systemctl enable nginx
+              sudo systemctl start nginx
+              EOF
+    key_name = aws_key_pair.nginx-server-ssh.key_name
+    vpc_security_group_ids = [
+        aws_security_group.nginx-server-sg.id
+    ]
+}
+####### security group #####
+resource "aws_security_group" "nginx-server-sg" {
+  name        = "nginx-server-sg"
+  description = "Security group allowing SSH and HTTP access"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+####### ssh ####### 
+####ssh-keygen -t rsa -b 2048 -f "nginx-server.key"
+
+resource "aws_key_pair" "nginx-server-ssh" {
+  key_name   = "nginx-server-ssh"
+  public_key = file("nginx-server.key.pub")
 }
